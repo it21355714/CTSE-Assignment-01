40e5e92cb491b733d743ae4593295e4753ea69be
Tharinduni Herath <tharinduni.medicode455@gmail.com>
2025-04-18 21:59:33 +0530

    Docker-compose creation

--- product-service/src/main/java/com/ctse/microservices/product/dto/ProductRequest.java
+++
@@ -1,8 +0,0 @@
-package com.ctse.microservices.product.dto;
-
-import java.math.BigDecimal;
-
-public record ProductRequest(String id, String name, String description, String categoryId, BigDecimal price) {
-    // This record will automatically generate the constructor, getters, and toString method
-    // No need for additional annotations or boilerplate code
-}
--- product-service/src/main/java/com/ctse/microservices/product/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ctse.microservices.product.repository;
-
-import com.ctse.microservices.product.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-    // Custom query methods can be defined here if needed
-    // For example, findByCategoryId(String categoryId);
-}
